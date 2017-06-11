c9cc5cd39c80d4d53bd5210057ae61fa03d563b0
St.Nann <mebook_qwerty33@hotmail.com>
2017-06-11 16:28:34 +0700

    responsive_mobile_splash_page

--- src/components/Splash.vue
+++ src/components/Splash.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="bg">
-    <img class="mobile mt-5 pt-5" src="../img/background/Splash-nobg.png" width="60%">
-    <v-layout>
+    <img class="mt-5 elevation-5" src="../img/background/Splash-nobg.png">
+    <v-layout row justify-center>
       <v-flex xs12>
-        <v-btn class="mt-4 px-5 white--text blue-grey darken-4" round dark>Enter Site</v-btn>
+        <v-btn class="mobile mt-4 px-5 white--text blue-grey darken-4" round dark>Enter Site</v-btn>
       </v-flex>
     </v-layout>
   </div>
@@ -28,5 +28,18 @@
   .flex-center {
     display: flex;
     justify-content: center;
+  }
+
+  @media only screen and (max-width: 375px) {
+    img {
+      width: 80%;
+    }
+
+    .mobile {
+      height: 20px;
+      font-size: 8px;
+      padding-left: 10px !important;
+      padding-right: 10px !important;
+    }
   }
 </style>
